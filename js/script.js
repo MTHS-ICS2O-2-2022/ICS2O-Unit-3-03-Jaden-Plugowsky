984b4d3592990b796bd9dafd4eb0411e6861aab5
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-04-12 01:57:31 +0000

    final website

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@
 "use strict"
 
 function calculatePressed() {
+  //This function calculates the volume of a Sphere
+  //Input through Textfields
+  const radius = parseFloat(document.getElementById("radius").value)
+
+  //Process
+  const volume = 4/3 * Math.PI * radius ** 3
+
+  //Output
   document.getElementById("answer").innerHTML =
-    "The answer is: " + "insert answer here"
+    "The Volume of the Sphere is: " + volume.toFixed(2) + "cm³"
 }
